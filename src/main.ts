f581693b5fc6105655646d2c5d54de733459de3a
Classbinu <classbinu@gmail.com>
2024-01-09 21:51:43 +0900

    도커 환경변수 테스트

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { swaggerConfig } from './swagger.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 환경 변수 읽어오기
+  const jwtAccessTokenSecret = process.env.JWT_ACCESS_TOKEN_SECRET;
+
+  // 환경 변수 로깅 또는 출력
+  console.log(`JWT_ACCESS_TOKEN_SECRET: ${jwtAccessTokenSecret}`);
+
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
   app.enableCors(); // 우선 전체 허용
